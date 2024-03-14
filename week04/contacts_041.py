42f056a99c2145ba8a9ca0f3634fc8f9ce0c79e4
Antonia <117043144+tuuniii@users.noreply.github.com>
2024-03-14 03:27:29 +0000

    Lab 4.1

---
+++ week04/contacts_041.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+file = sys.argv[1].strip()
+contacts = {}
+
+with open(file, "r") as f:
+    for line in f.readlines():
+        tokens = line.strip().split()
+        contacts[tokens[0]] = tokens[1]
+
+
+for line in sys.stdin:
+    line = line.strip()
+    if line in contacts:
+        print(f"Name: {line}")
+        print(f"Phone: {contacts[line]}")
+    else:
+        print(f"Name: {line}")
+        print("No such contact")
