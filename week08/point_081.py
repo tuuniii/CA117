fd5b5d5674676c5b9b8684f685c2fdc52f21728d
Antonia <117043144+tuuniii@users.noreply.github.com>
2024-03-14 03:35:52 +0000

    Lab 8.1

---
+++ week08/point_081.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Point():
+
+        def set_attributes(self, x, y):
+                self.x = x
+                self.y = y
+
+        def print_attributes(self):
+                print(f"x: {self.x:.2f}")
+                print(f"y: {self.y:.2f}")
+
+        def reflect(self):
+                self.x = self.x * (-1)
+                self.y = self.y * (-1)
